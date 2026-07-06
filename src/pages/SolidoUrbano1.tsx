4c12eb781f742c4baa35c1ae1dc581f003099b98
Jesus Gomez <imperio_jgu@hotmail.com>
2026-07-05 19:44:40 -0600

    ultimos cambios
    ultimos cambios

--- src/pages/SolidoUrbano1.tsx
+++ src/pages/SolidoUrbano1.tsx
@@ -443,7 +443,7 @@ export function SolidoUrbano1() {
                     </FieldLabel>
 
                     <Popover>
-                      <PopoverTrigger disabled>
+                      <PopoverTrigger >
                         <div
                           className="h-8 w-full cursor-pointer rounded-lg border border-input bg-transparent px-2.5 py-1 text-left text-base text-placeholder transition-colors outline-none focus-visible:border-ring focus-visible:ring-3 focus-visible:ring-ring/50 aria-invalid:border-destructive aria-invalid:ring-3 aria-invalid:ring-destructive/20 md:text-sm"
                           aria-invalid={fieldState.invalid}
